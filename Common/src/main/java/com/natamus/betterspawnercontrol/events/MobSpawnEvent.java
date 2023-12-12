8ee8191e7b7753da4f40b925555e16a5abd6a911
Rick South <rick@natamus.com>
2023-12-12 10:52:40 +0100

    Better Spawner Control source code for 1.18.2.

---
+++ Common/src/main/java/com/natamus/betterspawnercontrol/events/MobSpawnEvent.java
@@ -0,0 +1,48 @@
+package com.natamus.betterspawnercontrol.events;
+
+import com.natamus.collective.functions.BlockPosFunctions;
+import com.natamus.collective.functions.EntityFunctions;
+import com.natamus.collective.functions.TileEntityFunctions;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.TorchBlock;
+import net.minecraft.world.level.block.WallTorchBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+
+public class MobSpawnEvent {
+	public static boolean onMobSpawn(Mob entity, ServerLevel world, BlockPos spawnerPos, MobSpawnType spawnReason) {
+		if (EntityFunctions.isEntityFromSpawner(entity)) {
+			if (spawnerPos == null) {
+				return true;
+			}
+			
+			boolean alltorches = true;
+			for (BlockPos ap : BlockPosFunctions.getBlocksAround(spawnerPos, false)) {
+				Block block = world.getBlockState(ap).getBlock();
+				if (!(block instanceof TorchBlock) && !(block instanceof WallTorchBlock)) {
+					alltorches = false;
+					break;
+				}
+			}
+			
+			if (alltorches) {
+				BlockEntity blockentity = world.getBlockEntity(spawnerPos);
+				if (blockentity instanceof SpawnerBlockEntity) {
+					SpawnerBlockEntity sbe = (SpawnerBlockEntity)blockentity;
+					if (sbe != null) {
+						BaseSpawner basespawner = sbe.getSpawner();
+						TileEntityFunctions.resetMobSpawnerDelay(basespawner);
+					}
+				}
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
---
+++ Forge/src/main/java/com/natamus/betterspawnercontrol/forge/events/ForgeMobSpawnEvent.java
@@ -0,0 +1,38 @@
+package com.natamus.betterspawnercontrol.forge.events;
+
+import com.natamus.betterspawnercontrol.events.MobSpawnEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.eventbus.api.Event.Result;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeMobSpawnEvent {
+	@SubscribeEvent
+	public void onMobSpawn(LivingSpawnEvent.CheckSpawn e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getWorld());
+		if (level == null) {
+			return;
+		}
+
+		BaseSpawner msbl = e.getSpawner();
+		if (msbl != null) {
+			BlockEntity spawnerEntity = msbl.getSpawnerBlockEntity();
+			if (spawnerEntity == null) {
+				return;
+			}
+
+			BlockPos spawnerPos = spawnerEntity.getBlockPos();
+			if (!(MobSpawnEvent.onMobSpawn((Mob)e.getEntity(), (ServerLevel)level, spawnerPos, null))) {
+				e.setResult(Result.DENY);
+			}
+		}
+	}
+}
