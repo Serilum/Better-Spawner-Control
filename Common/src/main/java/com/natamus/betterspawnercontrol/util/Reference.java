190d2bf1c5c9ca582b1f774d5d2c26f72ac4e807
Rick South <rick@natamus.com>
2023-12-18 10:40:35 +0100

    (v4.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/betterspawnercontrol/util/Reference.java
+++ Common/src/main/java/com/natamus/betterspawnercontrol/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.betterspawnercontrol.util;
 public class Reference {
 	public static final String MOD_ID = "betterspawnercontrol";
 	public static final String NAME = "Better Spawner Control";
-	public static final String VERSION = "4.3";
+	public static final String VERSION = "4.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
