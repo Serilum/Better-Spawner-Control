f1f9155b5f456734856060f114e886fc4f0c4f77
Rick South <rick@natamus.com>
2024-04-28 16:11:48 +0200

    Better Spawner Control source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/betterspawnercontrol/neoforge/events/NeoForgeMobSpawnerEvent.java
@@ -0,0 +1,37 @@
+package com.natamus.betterspawnercontrol.neoforge.events;
+
+import com.natamus.betterspawnercontrol.events.MobSpawnerEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeMobSpawnerEvent {
+	@SubscribeEvent
+	public static void onMobSpawn(MobSpawnEvent.FinalizeSpawn e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		BaseSpawner msbl = e.getSpawner();
+		if (msbl != null) {
+			BlockEntity spawnerEntity = msbl.getSpawnerBlockEntity();
+			if (spawnerEntity == null) {
+				return;
+			}
+
+			BlockPos spawnerPos = spawnerEntity.getBlockPos();
+			if (!(MobSpawnerEvent.onMobSpawn(e.getEntity(), (ServerLevel)level, spawnerPos, null))) {
+				e.setSpawnCancelled(true);
+				e.setCanceled(true);
+			}
+		}
+	}
+}
