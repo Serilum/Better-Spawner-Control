23319753d70437921a2e1c5eea42ea80f779e5a9
Rick South <rick@natamus.com>
2024-05-26 14:57:39 +0200

    (v4.5) (1.20.6 Neo) Updated mod to work with NeoForge version 20.6.78+.

--- NeoForge/src/main/java/com/natamus/betterspawnercontrol/neoforge/events/NeoForgeMobSpawnerEvent.java
+++ NeoForge/src/main/java/com/natamus/betterspawnercontrol/neoforge/events/NeoForgeMobSpawnerEvent.java
@@ -1,20 +1,22 @@
 package com.natamus.betterspawnercontrol.neoforge.events;
 
+import com.mojang.datafixers.util.Either;
 import com.natamus.betterspawnercontrol.events.MobSpawnerEvent;
 import com.natamus.collective.functions.WorldFunctions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BaseSpawner;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 
 @EventBusSubscriber
 public class NeoForgeMobSpawnerEvent {
 	@SubscribeEvent
-	public static void onMobSpawn(MobSpawnEvent.FinalizeSpawn e) {
+	public static void onMobSpawn(MobSpawnEvent.PositionCheck e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
@@ -22,15 +24,24 @@ public class NeoForgeMobSpawnerEvent {
 
 		BaseSpawner msbl = e.getSpawner();
 		if (msbl != null) {
-			BlockEntity spawnerEntity = msbl.getSpawnerBlockEntity();
-			if (spawnerEntity == null) {
+			Either<BlockEntity, Entity> spawnerEntityEither = msbl.getOwner();
+			if (spawnerEntityEither == null) {
 				return;
 			}
 
-			BlockPos spawnerPos = spawnerEntity.getBlockPos();
+			BlockPos spawnerPos;
+			if (spawnerEntityEither.left().isPresent()) {
+				spawnerPos = spawnerEntityEither.left().get().getBlockPos();
+			}
+			else if (spawnerEntityEither.right().isPresent()) {
+				spawnerPos = spawnerEntityEither.right().get().blockPosition();
+			}
+			else {
+				return;
+			}
+
 			if (!(MobSpawnerEvent.onMobSpawn(e.getEntity(), (ServerLevel)level, spawnerPos, null))) {
-				e.setSpawnCancelled(true);
-				e.setCanceled(true);
+				e.setResult(MobSpawnEvent.PositionCheck.Result.FAIL);
 			}
 		}
 	}
