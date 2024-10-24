4cfefe47e0824c2f0108955cc592a604fa90a948
Rick South <rick@natamus.com>
2024-10-24 20:12:57 +0200

    Better Spawner Control source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/betterspawnercontrol/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.betterspawnercontrol.util;
+
+public class Reference {
+	public static final String MOD_ID = "betterspawnercontrol";
+	public static final String NAME = "Better Spawner Control";
+	public static final String VERSION = "4.6";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
