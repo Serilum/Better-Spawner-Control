cda813536d0b075f43413acb3639db3d07d8de31
Rick South <rick@natamus.com>
2026-06-19 00:11:01 +0200

    Updated Better Spawner Control to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/betterspawnercontrol/util/Reference.java
+++ Common/src/main/java/com/natamus/betterspawnercontrol/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "betterspawnercontrol";
 	public static final String NAME = "Better Spawner Control";
 	public static final String VERSION = "4.8";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
